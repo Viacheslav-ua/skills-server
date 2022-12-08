f0d1bf07009f699d52dc4e687987993c0bf9a67e
Viacheslav-ua <atph@ukr.net>
2022-12-08 22:06:21 +0200

    modification users.entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,12 +1,24 @@
 import {
+  Column,
   CreateDateColumn,
+  Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity('users')
 export class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ nullable: false })
+  login: string;
+
+  @Column({ nullable: false })
+  passwordHash: string;
+
+  @Column({ nullable: true })
+  email?: string;
 
   @CreateDateColumn()
   createdAt: Date;
