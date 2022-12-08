f18eb50a2e6bc84b4884d181118f1f37d648d67b
Viacheslav-ua <atph@ukr.net>
2022-12-08 21:36:45 +0200

    adding and implements cross-env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
