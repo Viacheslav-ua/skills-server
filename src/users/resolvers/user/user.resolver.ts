e75e971656833b0ba5ab0b8d500a7e2200051de2
Viacheslav-ua <atph@ukr.net>
2022-12-09 12:26:09 +0200

    create mutation methods in user.resolver

--- src/users/resolvers/user/user.resolver.ts
+++ src/users/resolvers/user/user.resolver.ts
@@ -1,4 +1,29 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UserEntity } from 'src/users/entities/users.entity';
+import { CreateUserInput } from 'src/users/inputs/create-user.input';
+import { UpdateUserInput } from 'src/users/inputs/update-user.input';
+import { UserService } from 'src/users/services/user/user.service';
 
-@Resolver()
-export class UserResolver {}
+@Resolver('User')
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => UserEntity)
+  async createUser(
+    @Args('createUser') createUserInput: CreateUserInput,
+  ): Promise<UserEntity> {
+    return await this.userService.createUser(createUserInput);
+  }
+
+  @Mutation(() => UserEntity)
+  async updateUser(
+    @Args('updateUser') updateUserInput: UpdateUserInput,
+  ): Promise<UserEntity> {
+    return await this.userService.updateUser(updateUserInput);
+  }
+
+  @Mutation(() => Number)
+  async removeUser(@Args('id') id: number): Promise<number> {
+    return await this.userService.removeUser(id);
+  }
+}
