346a12aa571e0930df4ac8e6f6f7def5c8e7b4d3
Viacheslav-ua <atph@ukr.net>
2022-12-09 23:32:36 +0200

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Phone-book server')
+    .setDescription('REST API Documentation')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api/docs', app, document);
+
   const config = app.get(ConfigService);
   const port = config.get<number>('API_PORT') || 3000;
 
