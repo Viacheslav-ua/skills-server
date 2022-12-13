fe46d7b8c90792111d69ff41345afd353a62d618
Viacheslav-ua <atph@ukr.net>
2022-12-13 13:29:41 +0200

    adding create func in post.service

--- src/posts/controller/posts.controller.ts
+++ src/posts/controller/posts.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile } from '@nestjs/common';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { PostsService } from '../services/posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private postService: PostsService) {}
+
+  @Post()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  createPost(@Body() createPostDto: CreatePostDto, @UploadedFile() image) {}
+}
--- src/posts/services/posts.service.ts
+++ src/posts/services/posts.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { Post } from '../entities/posts.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+  ) {}
+
+  async create(createPostDto: CreatePostDto, image: any) {
+    const fileName = 'jhg';
+    const post = await this.postRepository.save({
+      ...createPostDto,
+      image: fileName,
+    });
+    return post;
+  }
+}
